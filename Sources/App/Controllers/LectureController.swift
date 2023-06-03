125f2b441651b294cb442c52742d9c1c62edea3c
Andrew Teliguzov <wapsthefast@gmail.com>
2023-06-03 19:11:44 +0300

    added requestst for teachers and users

--- Sources/App/Controllers/LectureController.swift
+++ Sources/App/Controllers/LectureController.swift
@@ -20,31 +20,49 @@ struct LectureController: RouteCollection {
             lecture.delete(use: delete)
         }
         tokenAuthGroup.get(":groupID", use: getForGroup)
+        tokenAuthGroup.get("forUser", use : getForUser)
+        tokenAuthGroup.get("forStudent", use : getForStudent)
+        tokenAuthGroup.get("checkStudents", ":lectureID", use: checkStudents)
+        tokenAuthGroup.get("checkForStudent", use: checkForStudent)
+        tokenAuthGroup.get("regenerate", ":lectureID", use: regenerateCode)
+        tokenAuthGroup.get("one",":lectureID", use: getOne)
+        tokenAuthGroup.get("getStudentsForLecture", ":lectureID", use: getStudents)
+        tokenAuthGroup.get("mark", ":code", use: markStudentOnLecture)
     }
 
     func index(req: Request) async throws -> [Lecture] {
         try await Lecture.query(on: req.db).all()
     }
 
+    func getOne(req: Request) async throws -> Lecture{
+        guard let lecture = try await Lecture.find(req.parameters.get("lectureID"), on: req.db) else{
+            throw Abort(.notFound)
+        }
+        return lecture
+    }
+
     func create(req: Request) async throws -> Lecture {
         let lecture = try req.content.decode(Lecture.self)
-
-    
-        
         guard let group = try await Group.find(lecture.$group.id, on: req.db) else{
             throw Abort(.notAcceptable)
         }
-
+        lecture.code = UUID()
         try await lecture.save(on: req.db)
-         
         let users : [User] = try await group.$users.get(on: req.db)
         let students : [User] = users.filter{$0.role == .student}
-
         for student in students{
             let studentOnLecture = StudentsOnLecture(state: "notOnLecture", groupID: group.id!, lectureID: lecture.id!, userID: student.id!)
             try await studentOnLecture.save(on: req.db)
         }
+        return lecture
+    }
 
+    func regenerateCode(req : Request) async throws -> Lecture{
+        guard let lecture = try await Lecture.find(req.parameters.get("lectureID"), on: req.db) else{
+            throw Abort(.notFound)
+        }
+        lecture.code = UUID()
+        try await lecture.save(on: req.db)
         return lecture
     }
     
@@ -58,11 +76,104 @@ struct LectureController: RouteCollection {
             .filter(\.$group.$id == group.id!).all()
         return lectures
     }
+
+    func getForUser(req: Request) async throws -> [Lecture]{
+        let user = try req.auth.require(User.self)
+        let groups = try await user.$groups.get(on: req.db)
+        var lectures: [Lecture] = []
+        for group in groups {
+            let lecturesToAdd = try await Lecture.query(on: req.db)
+            .filter(\.$user.$id == user.id!)
+            .filter(\.$group.$id == group.id!).all()
+            lectures.append(contentsOf: lecturesToAdd)
+        }
+        return lectures
+    }
+
+
     
-//    func getForStudent(req: Request) async throws -> [Lecture]{
-//        let user = try req.auth.require(User.self)
-//
-//    }
+   func getForStudent(req: Request) async throws -> [Lecture]{
+       let user = try req.auth.require(User.self)
+        let groups = try await user.$groups.get(on: req.db)
+        var lectures: [Lecture] = []
+        for group in groups {
+            let lecturesToAdd = try await Lecture.query(on: req.db)
+            .filter(\.$group.$id == group.id!).all()
+            lectures.append(contentsOf: lecturesToAdd)
+        }
+        return lectures
+   }
+
+    func checkForStudent(req: Request) async throws -> [StudentsOnLecture]{
+        let user = try req.auth.require(User.self)
+        // guard let lecture = try await Lecture.find(req.parameters.get("lectureID"), on: req.db) else{
+        //     throw Abort(.notFound)
+        // }
+        //let studentOnLecture = try await StudentsOnLecture.find(req.parameters.get("lectureID"), on: req.db)
+        let studentOnLecture = try await StudentsOnLecture.query(on: req.db)
+        .filter(\.$user.$id == user.id!)
+        //.filter(\.$lecture.$id == lecture.id!)
+         .all() 
+        //else{
+        //     throw Abort(.notFound)
+        // }
+        return studentOnLecture
+   }
+
+   func checkStudents(req: Request) async throws -> [StudentsOnLecture]{
+        //let user = try req.auth.require(User.self)
+        guard let lecture = try await Lecture.find(req.parameters.get("lectureID"), on: req.db) else{
+            throw Abort(.notFound)
+        }
+        // let studentOnLecture = try await StudentsOnLecture.find(req.parameters.get("lectureID"), on: req.db)
+        let studentOnLecture = try await StudentsOnLecture.query(on: req.db)
+        //.filter(\.$user.$id == user.id!)
+        .filter(\.$lecture.$id == lecture.id!)
+        .all()
+        return studentOnLecture
+   }
+
+   func getStudents(req: Request) async throws -> [User] {
+    guard let lecture = try await Lecture.find(req.parameters.get("lectureID"), on: req.db) else {
+        throw Abort(.notFound)
+    }
+    
+    let studentOnLecture = try await StudentsOnLecture.query(on: req.db)
+        .filter(\.$lecture.$id == lecture.id!)
+        .all()
+    
+    var students: [User] = []
+    for student in studentOnLecture {
+        if let user = try await User.find(student.$user.id, on: req.db) {
+            students.append(user)
+        }
+    }
+    
+    return students
+}
+
+    func markStudentOnLecture(req: Request) async throws -> StudentsOnLecture{
+        let user = try req.auth.require(User.self)
+        guard let lecture = try await Lecture.query(on: req.db)
+        .filter(\.$code == req.parameters.get("code"))
+        .first() else{
+            throw Abort(.notFound)
+        }
+        guard let studentOnLecture = try await StudentsOnLecture.query(on: req.db)
+        .filter(\.$lecture.$id == lecture.id!)
+        .filter(\.$user.$id == user.id!)
+        .first() else{
+            throw Abort(.notFound)
+        }
+        if lecture.date+(600) < Date.now{
+            studentOnLecture.state = .late
+        }else{
+            studentOnLecture.state = .onLecture
+        }
+        try await studentOnLecture.save(on: req.db)
+        return studentOnLecture
+    }
+
 
     func delete(req: Request) async throws -> HTTPStatus {
         guard let lecture = try await Lecture.find(req.parameters.get("lectureID"), on: req.db) else {
