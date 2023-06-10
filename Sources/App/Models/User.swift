3cb12bbee08b974d1f52e80fd322129c5485fcbd
Andrew Teliguzov <wapsthefast@gmail.com>
2023-06-10 23:26:30 +0300

    Session Authenticate

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -1,6 +1,6 @@
 //
 //  User.swift
-//  
+//
 //
 //  Created by Андрэй Целігузаў on 24.04.23.
 //
@@ -8,89 +8,91 @@
 import Fluent
 import Vapor
 
-
-
 final class User: Model, Content {
-    static let schema = "users"
-    
-    @ID(key: .id)
+  static let schema = "users"
+
+  @ID(key: .id)
+  var id: UUID?
+
+  @Children(for: \.$user)
+  var lectures: [Lecture]
+
+  @Children(for: \.$user)
+  var userOnLecture: [StudentsOnLecture]
+
+  @Children(for: \.$user)
+  var questions: [Questions]
+
+  @Children(for: \.$user)
+  var answers: [Answers]
+
+  @Siblings(through: UserGroup.self, from: \.$user, to: \.$group)
+  var groups: [Group]
+
+  @Field(key: "name")
+  var name: String
+
+  @Field(key: "surname")
+  var surname: String
+
+  @Enum(key: "role")
+  var role: Role
+
+  @Field(key: "email")
+  var email: String
+
+  @Field(key: "username")
+  var username: String
+
+  @Field(key: "password")
+  var password: String
+
+  init() {}
+
+  init(
+    id: UUID? = nil, name: String, surname: String, role: String, email: String, username: String,
+    password: String
+  ) {
+    self.id = id
+    self.name = name
+    self.surname = surname
+    if role == "administrator" {
+      self.role = .administrator
+    } else if role == "teacher" {
+      self.role = .teacher
+    } else if role == "student" {
+      self.role = .student
+    }
+    self.email = email
+    self.username = username
+    self.password = password
+  }
+
+  final class Public: Content {
     var id: UUID?
-    
-    @Children(for: \.$user)
-    var lectures : [Lecture]
-    
-    @Children(for: \.$user)
-    var userOnLecture : [StudentsOnLecture]
-    
-    @Children(for: \.$user)
-    var questions : [Questions]
-    
-    @Children(for: \.$user)
-    var answers : [Answers]
-    
-    @Siblings(through: UserGroup.self, from: \.$user, to: \.$group)
-    var groups : [Group]
-
-    @Field(key: "name")
-    var name: String
-    
-    @Field(key: "surname")
-    var surname: String
-    
-    @Enum(key: "role")
-    var role: Role
-    
-    @Field(key: "email")
-    var email: String
-    
-    @Field(key: "username")
     var username: String
-    
-    @Field(key: "password")
-    var password: String
+    var name: String
+    var surname: String
+    var email: String
+    var role: Role
 
-    init() { }
+    init(id: UUID?, username: String, name: String, surname: String, email: String, role: Role) {
+      self.id = id
+      self.username = username
+      self.name = name
+      self.surname = surname
+      self.email = email
+      self.role = role
+    }
+  }
 
-    init(id: UUID? = nil, name: String, surname: String, role: String, email : String, username : String, password : String) {
-        self.id = id
-        self.name = name
-        self.surname = surname
-        if role == "administrator"{
-            self.role = .administrator
-        }else if role == "teacher"{
-            self.role = .teacher
-        }else if role == "student"{
-            self.role = .student
-        }
-        self.email = email
-        self.username = username
-        self.password = password
-    }
-    
-    final class Public : Content{
-        var id: UUID?
-        var username: String
-        var name : String
-        var surname : String
-        var email : String
-        var role : Role
-        
-        init(id: UUID?, username: String, name : String, surname : String, email : String, role : Role){
-            self.id = id
-            self.username = username
-            self.name = name
-            self.surname = surname
-            self.email = email
-            self.role = role
-        }
-    }
-    
 }
 
-extension User{
-    func convertToPublic()-> User.Public{
-        return User.Public(id: id, username: username, name : name, surname : surname, email : email, role : role)
-    }
+extension User {
+  func convertToPublic() -> User.Public {
+    return User.Public(
+      id: id, username: username, name: name, surname: surname, email: email, role: role)
+  }
 }
 
 extension EventLoopFuture where Value: User {
@@ -98,36 +100,37 @@ extension EventLoopFuture where Value: User {
   func convertToPublic() -> EventLoopFuture<User.Public> {
 
     return self.map { user in
-  
+
       return user.convertToPublic()
     }
   }
 }
 
-
 extension Collection where Element: User {
 
   func convertToPublic() -> [User.Public] {
-  
+
     return self.map { $0.convertToPublic() }
   }
 }
 
-
-extension EventLoopFuture where Value == Array<User> {
+extension EventLoopFuture where Value == [User] {
 
   func convertToPublic() -> EventLoopFuture<[User.Public]> {
-    
+
     return self.map { $0.convertToPublic() }
   }
 }
 
-extension User : ModelAuthenticatable{
-    static let usernameKey = \User.$username
-    
-    static let passwordHashKey = \User.$password
-    
-    func verify(password: String) throws -> Bool{
-        try Bcrypt.verify(password, created: self.password)
-    }
+extension User: ModelAuthenticatable {
+  static let usernameKey = \User.$username
+
+  static let passwordHashKey = \User.$password
+
+  func verify(password: String) throws -> Bool {
+    try Bcrypt.verify(password, created: self.password)
+  }
 }
+
+extension User: ModelSessionAuthenticatable {}
+extension User: ModelCredentialsAuthenticatable {}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,37 +5,37 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    app.routes.defaultMaxBodySize = "10mb" // установите достаточно большое значение
+  // uncomment to serve files from /Public folder
+  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  app.routes.defaultMaxBodySize = "10mb"  // установите достаточно большое значение
+  app.middleware.use(app.sessions.middleware)
 
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+  app.databases.use(
+    .postgres(
+      hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+      port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:))
+        ?? PostgresConfiguration.ianaPortNumber,
+      username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+      password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+      database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .psql)
 
-    
+  app.views.use(.leaf)
+  // app.migrations.add(CreateRole())
+  app.migrations.add(CreateUser())
+  app.migrations.add(CreateToken())
+  app.migrations.add(CreateAdminUser())
+  app.migrations.add(CreateGroup())
+  app.migrations.add(CreateUserGroupPivot())
+  app.migrations.add(CreateSubject())
+  app.migrations.add(CreateLecture())
+  app.migrations.add(CreateStudentsOnLecture())
+  app.migrations.add(CreateQuestions())
+  app.migrations.add(CreateAnswers())
+  app.migrations.add(CreateFeedback())
 
-    app.views.use(.leaf)
-   // app.migrations.add(CreateRole())
-    app.migrations.add(CreateUser())
-    app.migrations.add(CreateToken())
-    app.migrations.add(CreateAdminUser())
-    app.migrations.add(CreateGroup())
-    app.migrations.add(CreateUserGroupPivot())
-    app.migrations.add(CreateSubject())
-    app.migrations.add(CreateLecture())
-    app.migrations.add(CreateStudentsOnLecture())
-    app.migrations.add(CreateQuestions())
-    app.migrations.add(CreateAnswers())
-    app.migrations.add(CreateFeedback())
-    
-    try app.autoMigrate().wait()
+  try app.autoMigrate().wait()
 
-    // register routes
-    try routes(app)
+  // register routes
+  try routes(app)
 }
